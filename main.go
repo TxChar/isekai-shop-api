fff0e49ffe59780230ca92ec6052265a7e825b1f
TxChar <pontogreen1@gmail.com>
2025-03-02 02:03:14 +0700

    feat: 🎸 create server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/TxChar/isekai-shop-api/config"
+	"github.com/TxChar/isekai-shop-api/databases"
+	"github.com/TxChar/isekai-shop-api/server"
+)
 
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }
