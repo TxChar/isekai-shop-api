46308e472c7cda4797ffb746f5711ee4916a1c55
TxChar <pontogreen1@gmail.com>
2025-03-02 02:17:00 +0700

    feat: 🎸 create Middleware

--- server/server.go
+++ server/server.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/TxChar/isekai-shop-api/config"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"gorm.io/gorm"
 )
@@ -38,6 +40,19 @@ func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
 }
 
 func (s *echoServer) Start() {
+	// Initialize all middlewares
+	corsMiddleware := getCORSMiddleware(s.conf.Server.AllowOrigins)
+	bodyLimitMiddleware := getBodyLimitMiddleware(s.conf.Server.BodyLimit)
+	timeOutMiddleware := getTimeOutMiddleware(s.conf.Server.Timeout)
+
+	// Prevent application from crashing
+	s.app.Use(middleware.Recover())
+
+	s.app.Use(middleware.Logger())
+	s.app.Use(timeOutMiddleware)
+	s.app.Use(corsMiddleware)
+	s.app.Use(bodyLimitMiddleware)
+	
 	s.app.GET("/v1/health", s.healthCheck)
 	s.httpListening()
 }
@@ -52,4 +67,29 @@ func (s *echoServer) httpListening() {
 
 func (s *echoServer) healthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
+}
+
+// func getLoggerMiddleware() echo.MiddlewareFunc {
+// 	return middleware.Logger()
+// }
+
+func getTimeOutMiddleware(timeout time.Duration) echo.MiddlewareFunc {
+	return middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+		Skipper: middleware.DefaultSkipper,
+		ErrorMessage: "Request Timeout",
+		Timeout: timeout * time.Second,
+	})
+}
+
+func getCORSMiddleware(allowOrigins []string) echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper:      middleware.DefaultSkipper,
+		AllowOrigins: allowOrigins,
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	})
+}
+
+func getBodyLimitMiddleware(bodyLimit string) echo.MiddlewareFunc {
+	return middleware.BodyLimit(bodyLimit)
 }
